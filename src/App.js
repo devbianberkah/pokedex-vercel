dca378a6e2f0aee5a10c38af282deab2c4101846
sukmasuk2006 <sukmasuk2006@gmail.com>
2022-04-28 10:45:24 +0700

    Membuat Components untuk Halaman Home Setting css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import PageContent from './Components/PageContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <main className='page'>
+          <h1 className='page-title'>Pokedex</h1>
+          <span></span>
+      </main>
+      <PageContent />
     </div>
   );
 }
